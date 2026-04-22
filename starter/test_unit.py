fc59333e89964225fb49cab8e938f368af9504ba
Eoin <eoinkeohane98@gmail.com>
2026-04-22 17:34:02 +0100

    Began writing unit tests

---
+++ starter/test_unit.py
@@ -0,0 +1,13 @@
+import pytest
+
+import pandas as pd
+from ml.data import process_data
+from ml.model import train_model
+from ml.model import inference
+
+@pytest.fixture():
+def sample_data():
+    return pd.read_csv(pd.read_csv("./census.csv"), nrows=5000)
+
+def test_process_data(sample_data):
+    assert len(sample_data) == 5000
