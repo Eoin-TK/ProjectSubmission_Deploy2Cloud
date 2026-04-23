e77930f46161d3a0b5255ba9e531adad6c06762d
Eoin <eoinkeohane98@gmail.com>
2026-04-23 16:08:55 +0100

    added unit tests

--- test_unit.py
+++ test_unit.py
@@ -1,6 +1,7 @@
 import pytest
 
 import pandas as pd
+from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
 from ml.data import process_data
 #from ml.model import train_model
 #from ml.model import inference
@@ -9,8 +10,9 @@ from ml.data import process_data
 def sample_data():
     return pd.read_csv("./census.csv", nrows=5000)
 
-def test_process_data(sample_data):
-    cat_features = [
+@pytest.fixture
+def cat_features():
+    return [
         "workclass",
         "education",
         "marital-status",
@@ -20,10 +22,30 @@ def test_process_data(sample_data):
         "sex",
         "native-country"
     ]
-    X, y, encoder, lb = process_data(
-        sample_data, categorical_features=cat_features,
-        label="salary",
-        training=True
-    )
 
-    assert len(X) == len(sample_data)
+class TestProcessData:
+    def __init__(self, sample_data, cat_features):
+        self.f_in = sample_data
+        self.f_out_X, self.f_out_y, self.f_out_encoder, self.f_out_lb = process_data(
+            sample_data, categorical_features=cat_features,
+            label="salary",
+            training=True
+        )
+
+    def test_one(self):
+        assert isinstance(self.f_out_X, pd.DataFrame)
+
+    def test_two(self):
+        assert isinstance(self.f_out_y, pd.DataFrame)
+
+    def test_three(self):
+        assert isinstance(self.f_out_encoder, OneHotEncoder)
+
+    def test_four(self):
+        assert isinstance(self.f_out_lb, LabelBinarizer) 
+
+    def test_five(self):
+        assert len(self.f_out_X) == len(self.f_in)
+
+    def test_six(self):
+        assert len(self.f_out_y) == len(self.f_out_X)
