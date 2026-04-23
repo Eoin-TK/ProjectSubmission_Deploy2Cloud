d637a073c69d50249a786930fc2b7a8e90e4e5fb
Eoin <eoinkeohane98@gmail.com>
2026-04-23 15:43:41 +0100

    updating unit tests

--- test_unit.py
+++ test_unit.py
@@ -1,7 +1,7 @@
 import pytest
 
 import pandas as pd
-#from ml.data import process_data
+from ml.data import process_data
 #from ml.model import train_model
 #from ml.model import inference
 
@@ -10,4 +10,20 @@ def sample_data():
     return pd.read_csv("./census.csv", nrows=5000)
 
 def test_process_data(sample_data):
-    assert len(sample_data) == 5000
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country"
+    ]
+    X, y, encoder, lb = process_data(
+        sample_data, categorical_features=cat_features,
+        label="salary",
+        training=True
+    )
+
+    assert len(X) == len(sample_data)
