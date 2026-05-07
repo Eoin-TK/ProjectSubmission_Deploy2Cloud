6f6d98ec30a9b85a4937e5045a89353015d4ed92
Eoin <eoinkeohane98@gmail.com>
2026-05-07 12:20:12 +0100

    added script to demo live API (render)

---
+++ live_api_demo.py
@@ -0,0 +1,27 @@
+import requests
+
+if __name__ == "__main__":
+    # Example POST request
+    payload = {
+        "age": 52,
+        "workclass": "Self-emp-not-inc",
+        "fnlgt": 209642,
+        "education": "HS-grad",
+        "education-num": 9,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Husband",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 0,
+        "capital-loss": 0,
+        "hours-per-week": 45,
+        "native-country": "United-States"
+    }
+
+    try:
+        response = requests.post("https://projectsubmission-deploy2cloud.onrender.com/model_inference", json=payload)
+        response.raise_for_status()
+        print("POST Response:", response.json())
+    except requests.exceptions.RequestException as e:
+        print("Request failed:", e)
